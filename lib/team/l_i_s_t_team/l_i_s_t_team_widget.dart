9b9993dbb62acfa4c9c43432418a4bc4e254141a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-18 15:52:01 +0000

    Updating to latest FlutterFlow output.

--- lib/team/l_i_s_t_team/l_i_s_t_team_widget.dart
+++ lib/team/l_i_s_t_team/l_i_s_t_team_widget.dart
@@ -98,7 +98,10 @@ class _LISTTeamWidgetState extends State<LISTTeamWidget> {
                           ClipRRect(
                             borderRadius: BorderRadius.circular(5.0),
                             child: Image.network(
-                              listViewTeamRow.logo!,
+                              valueOrDefault<String>(
+                                listViewTeamRow.logo,
+                                'false',
+                              ),
                               width: 50.0,
                               height: 50.0,
                               fit: BoxFit.cover,
