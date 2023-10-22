27d434dfb2405d40facebd7d6a23981633857f9b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-22 11:40:22 +0000

    Updating to latest FlutterFlow output.

--- lib/messages/l_i_s_t_chat/l_i_s_t_chat_widget.dart
+++ lib/messages/l_i_s_t_chat/l_i_s_t_chat_widget.dart
@@ -1,4 +1,3 @@
-import '/auth/supabase_auth/auth_util.dart';
 import '/backend/supabase/supabase.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -84,10 +83,7 @@ class _LISTChatWidgetState extends State<LISTChatWidget> {
                 padding: EdgeInsetsDirectional.fromSTEB(15.0, 15.0, 15.0, 15.0),
                 child: FutureBuilder<List<ChatRow>>(
                   future: ChatTable().queryRows(
-                    queryFn: (q) => q.contains(
-                      'Companions',
-                      '{' + currentUserUid + '}',
-                    ),
+                    queryFn: (q) => q,
                   ),
                   builder: (context, snapshot) {
                     // Customize what your widget looks like when it's loading.
